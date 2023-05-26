aa3de73378e96bbac60502072f5f610f8e53ba61
Kaushik Surya <kausyas@amazon.com>
2023-05-26 15:38:21 -0400

    Revert "Disable failing test"
    This reverts commit 758169c0043e8b9f62d0ec33027f7a18fc057c74.

--- receiver/awscontainerinsightreceiver/internal/k8sapiserver/prometheus_scraper_test.go
+++ receiver/awscontainerinsightreceiver/internal/k8sapiserver/prometheus_scraper_test.go
@@ -16,10 +16,20 @@ package k8sapiserver
 
 import (
 	"context"
+	"strings"
+	"testing"
+	"time"
+
 	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/collector/component/componenttest"
+	"go.opentelemetry.io/collector/config/confighttp"
+	"go.opentelemetry.io/collector/config/configtls"
 	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/pdata/pmetric"
-	"testing"
+	"go.opentelemetry.io/collector/receiver"
+	"go.uber.org/zap"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/simpleprometheusreceiver"
 )
 
 const renameMetric = `
@@ -71,69 +81,69 @@ func (m mockConsumer) ConsumeMetrics(ctx context.Context, md pmetric.Metrics) er
 }
 func TestNewPrometheusScraperEndToEnd(t *testing.T) {
 
-	//upPtr := false
-	//httpPtr := false
-	//
-	//consumer := mockConsumer{
-	//	t:             t,
-	//	up:            &upPtr,
-	//	httpConnected: &httpPtr,
-	//}
-	//
-	//settings := componenttest.NewNopTelemetrySettings()
-	//settings.Logger, _ = zap.NewDevelopment()
-	//
-	//scraper, err := NewPrometheusScraper(context.TODO(), settings, consumer, componenttest.NewNopHost(), mockClusterNameProvider{})
-	//assert.NoError(t, err)
-	//assert.Equal(t, mockClusterNameProvider{}, scraper.clusterNameProvider)
-	//
-	//// build up a new SPR
-	//spFactory := simpleprometheusreceiver.NewFactory()
-	//
-	//targets := []*testData{
-	//	{
-	//		name: "prometheus_simple",
-	//		pages: []mockPrometheusResponse{
-	//			{code: 200, data: renameMetric},
-	//		},
-	//	},
-	//}
-	//mp, cfg, err := setupMockPrometheus(targets...)
-	//assert.NoError(t, err)
-	//
-	//split := strings.Split(mp.srv.URL, "http://")
-	//
-	//spConfig := simpleprometheusreceiver.Config{
-	//	HTTPClientSettings: confighttp.HTTPClientSettings{
-	//		Endpoint: split[1],
-	//		TLSSetting: configtls.TLSClientSetting{
-	//			Insecure:           true,
-	//			InsecureSkipVerify: true,
-	//		},
-	//	},
-	//	MetricsPath:        cfg.ScrapeConfigs[0].MetricsPath,
-	//	CollectionInterval: time.Duration(cfg.ScrapeConfigs[0].ScrapeInterval),
-	//	UseServiceAccount:  false,
-	//}
-	//
-	//// replace the SPR
-	//params := receiver.CreateSettings{
-	//	TelemetrySettings: scraper.settings,
-	//}
-	//scraper.simplePrometheusReceiver, err = spFactory.CreateMetricsReceiver(scraper.ctx, params, &spConfig, consumer)
-	//assert.NoError(t, err)
-	//assert.NotNil(t, mp)
-	//defer mp.Close()
-	//
-	//scraper.Start()
-	//
-	//t.Cleanup(func() {
-	//	scraper.Shutdown()
-	//})
-	//
-	//// wait for scrape
-	//mp.wg.Wait()
-	//
-	//assert.True(t, *consumer.up)
-	//assert.True(t, *consumer.httpConnected)
+	upPtr := false
+	httpPtr := false
+
+	consumer := mockConsumer{
+		t:             t,
+		up:            &upPtr,
+		httpConnected: &httpPtr,
+	}
+
+	settings := componenttest.NewNopTelemetrySettings()
+	settings.Logger, _ = zap.NewDevelopment()
+
+	scraper, err := NewPrometheusScraper(context.TODO(), settings, consumer, componenttest.NewNopHost(), mockClusterNameProvider{})
+	assert.NoError(t, err)
+	assert.Equal(t, mockClusterNameProvider{}, scraper.clusterNameProvider)
+
+	// build up a new SPR
+	spFactory := simpleprometheusreceiver.NewFactory()
+
+	targets := []*testData{
+		{
+			name: "prometheus_simple",
+			pages: []mockPrometheusResponse{
+				{code: 200, data: renameMetric},
+			},
+		},
+	}
+	mp, cfg, err := setupMockPrometheus(targets...)
+	assert.NoError(t, err)
+
+	split := strings.Split(mp.srv.URL, "http://")
+
+	spConfig := simpleprometheusreceiver.Config{
+		HTTPClientSettings: confighttp.HTTPClientSettings{
+			Endpoint: split[1],
+			TLSSetting: configtls.TLSClientSetting{
+				Insecure:           true,
+				InsecureSkipVerify: true,
+			},
+		},
+		MetricsPath:        cfg.ScrapeConfigs[0].MetricsPath,
+		CollectionInterval: time.Duration(cfg.ScrapeConfigs[0].ScrapeInterval),
+		UseServiceAccount:  false,
+	}
+
+	// replace the SPR
+	params := receiver.CreateSettings{
+		TelemetrySettings: scraper.settings,
+	}
+	scraper.simplePrometheusReceiver, err = spFactory.CreateMetricsReceiver(scraper.ctx, params, &spConfig, consumer)
+	assert.NoError(t, err)
+	assert.NotNil(t, mp)
+	defer mp.Close()
+
+	scraper.Start()
+
+	t.Cleanup(func() {
+		scraper.Shutdown()
+	})
+
+	// wait for scrape
+	mp.wg.Wait()
+
+	assert.True(t, *consumer.up)
+	assert.True(t, *consumer.httpConnected)
 }
