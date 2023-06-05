557ce4fa708b004c6cede499c4ac5a7d12ba7e27
Kaushik Surya <kausyas@amazon.com>
2023-06-04 21:08:39 -0400

    Temp logging changes

--- receiver/awscontainerinsightreceiver/internal/cadvisor/container_info_processor.go
+++ receiver/awscontainerinsightreceiver/internal/cadvisor/container_info_processor.go
@@ -23,6 +23,7 @@ import (
 	"path"
 	"strconv"
 	"strings"
+	"time"
 
 	cInfo "github.com/google/cadvisor/info/v1"
 	"go.uber.org/zap"
@@ -50,7 +51,7 @@ type podKey struct {
 	namespace    string
 }
 
-func processContainers(cInfos []*cInfo.ContainerInfo, mInfo extractors.CPUMemInfoProvider, containerOrchestrator string, logger *zap.Logger) []*extractors.CAdvisorMetric {
+func processContainers(cInfos []*cInfo.ContainerInfo, mInfo extractors.CPUMemInfoProvider, containerOrchestrator string, logger *za) []*extractors.CAdvisorMetric {
 	var metrics []*extractors.CAdvisorMetric
 	podKeys := make(map[string]podKey)
 
@@ -112,14 +113,14 @@ func processContainer(info *cInfo.ContainerInfo, mInfo extractors.CPUMemInfoProv
 	tags := map[string]string{}
 
 	var containerType string
-	p.logger.Info(fmt.Sprintf("FINDME1-%d: Procssing cinfo with name: %s", random.Int(), info.Name))
+	logger.Info(fmt.Sprintf("FINDME1-%d: Procssing cinfo with name: %s", random.Int(), info.Name))
 	if info.Name != "/" {
 		// Only a container has all these three labels set.
 		containerName := info.Spec.Labels[containerNameLabel]
 		namespace := info.Spec.Labels[namespaceLabel]
 		podName := info.Spec.Labels[podNameLabel]
 		podID := info.Spec.Labels[podIDLabel]
-		p.logger.Info(fmt.Sprintf("FINDME1-%d: Details: Name:%s, Namespace:%s, PodName:%s, PodID:%s", random.Int(), containerName, namespace, podName, podID))
+		logger.Info(fmt.Sprintf("FINDME1-%d: Details: Name:%s, Namespace:%s, PodName:%s, PodID:%s", random.Int(), containerName, namespace, podName, podID))
 		// NOTE: containerName can be empty for pause container on containerd
 		// https://github.com/containerd/cri/issues/922#issuecomment-423729537
 		if namespace == "" || podName == "" {
@@ -145,12 +146,12 @@ func processContainer(info *cInfo.ContainerInfo, mInfo extractors.CPUMemInfoProv
 		// For docker, pause container name is set to POD while containerd does not set it.
 		// See https://github.com/aws/amazon-cloudwatch-agent/issues/188
 		case "", infraContainerName:
-			p.logger.Info(fmt.Sprintf("FINDME1-%d: Sending as InfraContainer with path: %s", random.Int(), podPath))
+			logger.Info(fmt.Sprintf("FINDME1-%d: Sending as InfraContainer with path: %s", random.Int(), podPath))
 			// NOTE: the pod here is only used by NetMetricExtractor,
 			// other pod info like CPU, Mem are dealt within in processPod.
 			containerType = ci.TypeInfraContainer
 		default:
-			p.logger.Info(fmt.Sprintf("FINDME1-%d: Sending as NonInfraContainer with path: %s", random.Int(), podPath))
+			logger.Info(fmt.Sprintf("FINDME1-%d: Sending as NonInfraContainer with path: %s", random.Int(), podPath))
 			tags[ci.ContainerNamekey] = containerName
 			containerID := path.Base(info.Name)
 			tags[ci.ContainerIDkey] = containerID
@@ -162,7 +163,7 @@ func processContainer(info *cInfo.ContainerInfo, mInfo extractors.CPUMemInfoProv
 			}
 		}
 	} else {
-		p.logger.Info(fmt.Sprintf("FINDME1-%d: Sending as Node with path: %s", random.Int(), podPath))
+		logger.Info(fmt.Sprintf("FINDME1-%d: Sending as Root"))
 		containerType = ci.TypeNode
 		if containerOrchestrator == ci.ECS {
 			containerType = ci.TypeInstance
